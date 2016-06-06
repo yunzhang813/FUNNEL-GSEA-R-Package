b21c3e196a4aa38a4f1f0f555dafadecb289d1b9
Xing Qiu <xing.qiu@gmail.com>
2016-06-05 20:27:26 -0400

    Deleted the unitest file for now.  Bumped version number to 0.1.0.

--- inst/unitTests/test_wMWUTest.R
+++
@@ -1,5 +0,0 @@
-test_wMWUTest <- function() {
-  data(sampleData)
-  tst <- wMWUTest(index, stat, weight=weight, correlation=0.1)
-  checkEqualsNumeric(as.vector(tst), c(0.97293933,0.02849134), tolerance=1.0e-4)
-}
